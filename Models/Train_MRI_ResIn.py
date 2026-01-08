a22f9dfbb17ff1ab30c02192655942e23b16514f
KermitXeno <elamreece@gmail.com>
2026-01-07 21:10:45 -0500

    add back sequential import after removing it by mistake

--- Models/Train_MRI_ResIn.py
+++ Models/Train_MRI_ResIn.py
@@ -14,7 +14,7 @@
 """
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import *
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import pyarrow.parquet as pq
