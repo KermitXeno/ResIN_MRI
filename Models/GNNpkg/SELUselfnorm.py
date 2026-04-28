91df1263d2526cf1b1a2723532a37312dc16686d
KermitXeno <elamreece@gmail.com>
2026-04-28 14:24:11 -0400

    fix bug that caused inf loss

--- Models/GNNpkg/SELUselfnorm.py
+++ Models/GNNpkg/SELUselfnorm.py
@@ -6,15 +6,12 @@ from tensorflow.keras import initializers
 
 _ALPHA_SELU = 1.6732632423543772
 _LAMBDA_SELU = 1.0507009873554805
-
 Q = 1.072
 BETA = 1.0 / math.sqrt(2.0)
 GAMMA = 1.0 / math.sqrt(Q)
 C_V = 0.783
-C_MAX_SQ = (_LAMBDA_SELU ** 2) * (1.0 + _ALPHA_SELU ** 2) / 2.0
 
-
-def _branch_grad_correction(x: tf.Tensor, gamma: float = GAMMA) -> tf.Tensor:
+def _branch_grad_correction(x, gamma=GAMMA):
     @tf.custom_gradient
     def _op(t):
         def _grad(dy):
@@ -22,206 +19,176 @@ def _branch_grad_correction(x: tf.Tensor, gamma: float = GAMMA) -> tf.Tensor:
         return tf.identity(t), _grad
     return _op(x)
 
-
 class OrthogonalLeCunInitializer(initializers.Initializer):
-    def __init__(self, seed: int = None):
+    def __init__(self, seed=None):
         self.seed = seed
-
     def __call__(self, shape, dtype=tf.float32):
-        n_out = int(shape[-1])
-        n_in = int(np.prod(shape[:-1]))
-
+        if len(shape) < 2:
+            raise ValueError(f"Shape must be at least 2D, got {shape}")
+        fan_in = int(np.prod(shape[:-1]))
+        fan_out = int(shape[-1])
         rng = np.random.default_rng(self.seed)
-        flat = rng.standard_normal((n_out, n_in)).astype(np.float32)
-
-        u, _, vt = np.linalg.svd(flat, full_matrices=False)
-        q = vt
-        q = q / math.sqrt(n_in)
-
-        return tf.cast(tf.reshape(q, shape), dtype)
-
+        if fan_in >= fan_out:
+            a = rng.standard_normal((fan_in, fan_in)).astype(np.float32)
+            Q, _ = np.linalg.qr(a)
+            W_2d = Q[:fan_in, :fan_out]
+        else:
+            std = 1.0 / math.sqrt(fan_in)
+            W_2d = rng.standard_normal((fan_in, fan_out)).astype(np.float32) * std
+        return tf.cast(tf.reshape(W_2d, shape), dtype)
     def get_config(self):
         return {"seed": self.seed}
 
-
 class ZCAWhiten(Layer):
-    def __init__(self, momentum: float = 0.99, epsilon: float = 1e-5, **kwargs):
+    def __init__(self, momentum=0.99, epsilon=1e-5, max_full_channels=128, **kwargs):
         super().__init__(**kwargs)
         self.momentum = momentum
         self.epsilon = epsilon
-
+        self.max_full_channels = max_full_channels
     def build(self, input_shape):
         c = int(input_shape[-1])
         self._c = c
-
+        self._full = (c <= self.max_full_channels)
         self.running_mean = self.add_weight(
-            name="running_mean", shape=(c,),
-            initializer="zeros", trainable=False
-        )
-        self.running_cov = self.add_weight(
-            name="running_cov", shape=(c, c),
-            initializer=tf.keras.initializers.Identity(),
-            trainable=False
-        )
+            name="running_mean", shape=(c,), initializer="zeros", trainable=False)
+        if self._full:
+            self.running_cov = self.add_weight(
+                name="running_cov", shape=(c, c),
+                initializer=tf.keras.initializers.Identity(), trainable=False)
+        else:
+            self.running_var = self.add_weight(
+                name="running_var", shape=(c,), initializer="ones", trainable=False)
         super().build(input_shape)
-
     def call(self, x, training=None):
-        input_shape = tf.shape(x)
         c = self._c
-
+        orig_shape = tf.shape(x)
         flat = tf.cast(tf.reshape(x, [-1, c]), tf.float32)
+        flat = tf.where(tf.math.is_finite(flat), flat, tf.zeros_like(flat))
+        flat = tf.clip_by_value(flat, -50.0, 50.0)
         n = tf.cast(tf.shape(flat)[0], tf.float32)
-
+        if self._full:
+            return self._full_zca(flat, orig_shape, n, training, x.dtype)
+        return self._diag_zca(flat, orig_shape, training, x.dtype)
+    def _full_zca(self, flat, orig_shape, n, training, dtype):
         if training:
             mu = tf.reduce_mean(flat, axis=0)
             centered = flat - mu
-            cov = tf.matmul(centered, centered, transpose_a=True) / (n - 1.0 + self.epsilon)
-
-            self.running_mean.assign(
-                self.momentum * self.running_mean + (1.0 - self.momentum) * mu
-            )
-            self.running_cov.assign(
-                self.momentum * self.running_cov + (1.0 - self.momentum) * cov
-            )
-
+            cov = (tf.matmul(centered, centered, transpose_a=True)
+                   / (n - 1.0 + self.epsilon)
+                   + self.epsilon * tf.eye(self._c))
+            cov = tf.where(tf.math.is_finite(cov), cov, tf.eye(self._c))
+            self.running_mean.assign(self.momentum * self.running_mean + (1.0 - self.momentum) * mu)
+            self.running_cov.assign(self.momentum * self.running_cov + (1.0 - self.momentum) * cov)
         centered = flat - self.running_mean
-
         eigvals, eigvecs = tf.linalg.eigh(self.running_cov)
         eigvals = tf.maximum(eigvals, self.epsilon)
         inv_sqrt = 1.0 / tf.sqrt(eigvals)
-
-        W_zca = eigvecs * inv_sqrt[tf.newaxis, :]
-        W_zca = tf.matmul(W_zca, eigvecs, transpose_b=True)
-        W_zca = tf.stop_gradient(W_zca)
-
+        W_zca = tf.stop_gradient(
+            tf.matmul(eigvecs * inv_sqrt[tf.newaxis, :], eigvecs, transpose_b=True))
         whitened = tf.matmul(centered, W_zca, transpose_b=True)
-        return tf.reshape(whitened, input_shape)
-
+        whitened = tf.where(tf.math.is_finite(whitened), whitened, tf.zeros_like(whitened))
+        return tf.reshape(tf.cast(whitened, dtype), orig_shape)
+    def _diag_zca(self, flat, orig_shape, training, dtype):
+        if training:
+            mu = tf.reduce_mean(flat, axis=0)
+            var = tf.math.reduce_variance(flat, axis=0)
+            var = tf.where(tf.math.is_finite(var), var, tf.ones_like(var))
+            self.running_mean.assign(self.momentum * self.running_mean + (1.0 - self.momentum) * mu)
+            self.running_var.assign(self.momentum * self.running_var + (1.0 - self.momentum) * var)
+        centered = flat - self.running_mean
+        std = tf.sqrt(tf.maximum(self.running_var, self.epsilon))
+        whitened = centered / std
+        whitened = tf.where(tf.math.is_finite(whitened), whitened, tf.zeros_like(whitened))
+        return tf.reshape(tf.cast(whitened, dtype), orig_shape)
     def get_config(self):
         cfg = super().get_config()
-        cfg.update({"momentum": self.momentum, "epsilon": self.epsilon})
+        cfg.update({"momentum": self.momentum, "epsilon": self.epsilon,
+                    "max_full_channels": self.max_full_channels})
         return cfg
 
-
 class SELUResidual(Layer):
-    def __init__(self, out_channels: int, stride: int = 1, dropout_rate: float = 0.05, **kwargs):
+    def __init__(self, out_channels, stride=1, dropout_rate=0.05, **kwargs):
         super().__init__(**kwargs)
         self.out_channels = out_channels
         self.stride = stride
         self.dropout_rate = dropout_rate
-
-        self.conv1 = Conv2D(
-            out_channels, 3, padding="same",
-            kernel_initializer="lecun_normal",
-            use_bias=False
-        )
-        self.conv2 = Conv2D(
-            out_channels, 3, strides=stride, padding="same",
-            kernel_initializer="lecun_normal",
-            use_bias=False
-        )
-
+        self.conv1 = Conv2D(out_channels, 3, padding="same",
+                            kernel_initializer="lecun_normal", use_bias=False)
+        self.conv2 = Conv2D(out_channels, 3, strides=stride, padding="same",
+                            kernel_initializer="lecun_normal", use_bias=False)
         self.dropout = AlphaDropout(dropout_rate)
         self._shortcut_proj = None
-
     def build(self, input_shape):
         in_c = int(input_shape[-1])
         if self.stride != 1 or in_c != self.out_channels:
-            self._shortcut_proj = Conv2D(
-                self.out_channels, 1, strides=self.stride, padding="same",
-                kernel_initializer="lecun_normal",
-                use_bias=False
-            )
+            self._shortcut_proj = Conv2D(self.out_channels, 1, strides=self.stride,
+                                         padding="same", kernel_initializer="lecun_normal",
+                                         use_bias=False)
         super().build(input_shape)
-
     def call(self, x, training=None):
+        x = tf.where(tf.math.is_finite(x), x, tf.zeros_like(x))
         h = tf.nn.selu(x)
         h = self.conv1(h)
         h = tf.nn.selu(h)
         h = self.conv2(h)
         h = self.dropout(h, training=training)
         h = _branch_grad_correction(h, gamma=GAMMA)
-
         s = x if self._shortcut_proj is None else self._shortcut_proj(x)
-
         return BETA * h + BETA * s
-
     def get_config(self):
         cfg = super().get_config()
-        cfg.update({
-            "out_channels": self.out_channels,
-            "stride": self.stride,
-            "dropout_rate": self.dropout_rate,
-        })
+        cfg.update({"out_channels": self.out_channels, "stride": self.stride,
+                    "dropout_rate": self.dropout_rate})
         return cfg
 
-
 class SELUInception(Layer):
-    def __init__(self, channels: int, zca_momentum: float = 0.99, dropout_rate: float = 0.0, **kwargs):
+    def __init__(self, channels, zca_momentum=0.99, zca_max_full_ch=128,
+                 dropout_rate=0.0, **kwargs):
         super().__init__(**kwargs)
         self.channels = channels
         self.zca_momentum = zca_momentum
+        self.zca_max_full_ch = zca_max_full_ch
         self.dropout_rate = dropout_rate
-
         _init = OrthogonalLeCunInitializer()
-
         self.b1x1 = Conv2D(channels, 1, padding="same", kernel_initializer=_init, use_bias=False)
         self.b3x3 = Conv2D(channels, 3, padding="same", kernel_initializer=_init, use_bias=False)
         self.b5x5 = Conv2D(channels, 5, padding="same", kernel_initializer=_init, use_bias=False)
         self.bpool = Conv2D(channels, 1, padding="same", kernel_initializer=_init, use_bias=False)
-
-        self.zca = ZCAWhiten(momentum=zca_momentum)
+        self.pool = MaxPooling2D(3, strides=1, padding="same")
+        self.zca = ZCAWhiten(momentum=zca_momentum, max_full_channels=zca_max_full_ch)
         self.dropout = AlphaDropout(dropout_rate) if dropout_rate > 0 else None
-        self._pool = None
-        self._skip = None
-
+        self._skip_proj = None
     def build(self, input_shape):
         in_c = int(input_shape[-1])
         out_c = 4 * self.channels
-
-        self._pool = MaxPooling2D(3, strides=1, padding="same")
-
         if in_c != out_c:
-            self._skip = Conv2D(
-                out_c, 1, padding="same",
-                kernel_initializer="lecun_normal",
-                use_bias=False
-            )
+            self._skip_proj = Conv2D(out_c, 1, padding="same",
+                                     kernel_initializer="lecun_normal", use_bias=False)
         super().build(input_shape)
-
     def call(self, x, training=None):
+        x = tf.where(tf.math.is_finite(x), x, tf.zeros_like(x))
         b1 = tf.nn.selu(self.b1x1(x))
         b3 = tf.nn.selu(self.b3x3(x))
         b5 = tf.nn.selu(self.b5x5(x))
-        bp = tf.nn.selu(self.bpool(self._pool(x)))
-
+        bp = tf.nn.selu(self.bpool(self.pool(x)))
         y = tf.concat([b1, b3, b5, bp], axis=-1)
-
         if self.dropout is not None:
             y = self.dropout(y, training=training)
-
         y = self.zca(y, training=training)
         y = _branch_grad_correction(y, gamma=GAMMA)
-
-        s = x if self._skip is None else self._skip(x)
-
+        s = x if self._skip_proj is None else self._skip_proj(x)
         return BETA * y + BETA * s
-
     def get_config(self):
         cfg = super().get_config()
-        cfg.update({
-            "channels": self.channels,
-            "zca_momentum": self.zca_momentum,
-            "dropout_rate": self.dropout_rate,
-        })
+        cfg.update({"channels": self.channels, "zca_momentum": self.zca_momentum,
+                    "zca_max_full_ch": self.zca_max_full_ch, "dropout_rate": self.dropout_rate})
         return cfg
 
-
-def block_contraction_rate(D: int) -> float:
+def block_contraction_rate(D):
     return (1.0 + C_V ** D) / 2.0
 
-
-def lecun_lr(D: int, L_ell: float = 1.0) -> float:
+def lecun_lr(D, L_ell=1.0):
+    C_MAX_SQ = (_LAMBDA_SELU ** 2) * (1.0 + _ALPHA_SELU ** 2) / 2.0
     kappa = block_contraction_rate(D)
     eta_s = 1.0 / (2.0 * L_ell * C_MAX_SQ ** (D / 2.0))
     eta_kappa = 3.0 * (1.0 - kappa ** 2) ** 2 / 8.0
--- Models/Train_MRI_ResInSELU.py
+++ Models/Train_MRI_ResInSELU.py
@@ -15,45 +15,47 @@ This implementation is not for production and is an exploration of self normaliz
 }
 
 """
-
-import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import *
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-import pyarrow.parquet as pq
-import numpy as np
-from PIL import Image
 import os
 import io
-from GNNpkg.SELUselfnorm import SELUInception, SELUResidual 
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, GlobalAveragePooling2D, Dense
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import pyarrow.parquet as pq
+from GNNpkg.SELUselfnorm import SELUInception, SELUResidual, lecun_lr
 
-
-#PIPELINE AND ALL PIPLINE PROBLEMS ARE IN THE GENERATOR AND PREPROCESSING
 def parquet_generator(table):
-    for idx, row in enumerate(table.to_pylist()):
+    for row in table.to_pylist():
+
         img = row["image"]
         label = row["label"]
 
         if isinstance(img, dict):
             if "bytes" in img:
                 img = Image.open(io.BytesIO(img["bytes"])).convert("RGB")
-            elif "data" in img and "shape" in img:
-                arr = np.array(img["data"], dtype = np.uint8)
-                arr = arr.reshape(img["shape"])
-                img = Image.fromarray(arr)
-            else:
-                raise ValueError(f"Unknown image dict format: {img.keys()}")
-        elif isinstance(img, (bytes, bytearray)):
-            img = Image.open(io.BytesIO(img))
-        else:
-            img = Image.fromarray(img)
 
-        img = np.array(img, dtype = np.float32)
+            elif "data" in img and "shape" in img:
+                arr = np.array(img["data"], dtype=np.uint8).reshape(img["shape"])
+                img = Image.fromarray(arr)
+
+            else:
+                raise ValueError(f"Unknown image dict format: {list(img.keys())}")
+
+        elif isinstance(img, (bytes, bytearray)):
+            img = Image.open(io.BytesIO(img)).convert("RGB")
+
+        else:
+            img = Image.fromarray(np.asarray(img))
+
+        img = np.array(img, dtype=np.float32)
 
         if img.ndim == 2:
-            img = np.stack([img] * 3, axis = -1)
+            img = np.stack([img] * 3, axis=-1)
+
         elif img.ndim == 3 and img.shape[-1] == 1:
-            img = np.repeat(img, 3, axis = -1)
+            img = np.repeat(img, 3, axis=-1)
 
         img = img / 255.0
 
@@ -61,83 +63,91 @@ def parquet_generator(table):
 
 def preprocess(x, y):
     x = tf.image.resize(x, (128, 128))
-    x = tf.image.per_image_standardization(x)
+    x = x * 2.0 - 1.0
     return x, y
+
+def build_model(num_classes):
+    inputs = Input(shape=(128, 128, 3))
+    x = Conv2D(128, 3, padding="same", kernel_initializer="lecun_normal", use_bias=False)(inputs)
+
+    x = SELUResidual(128)(x)
+    x = SELUResidual(128, stride=2)(x)
+    x = SELUInception(128)(x)
+
+    x = SELUResidual(128)(x)
+    x = SELUResidual(128, stride=2)(x)
+    x = SELUInception(128)(x)
+
+    x = SELUResidual(256)(x)
+    x = SELUResidual(256, stride=2)(x)
+    x = SELUInception(256)(x)
+
+    x = GlobalAveragePooling2D()(x)
+
+    outputs = Dense(num_classes, kernel_initializer="lecun_normal")(x)
+
+    return Model(inputs, outputs)
 
 def trainResinSELU():
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    dataloc = os.path.join(base_dir, 'data')
+    dataloc = os.path.join(base_dir, "data")
+    pathsave = os.path.join(base_dir, "weights", "MRISELUresin.keras")
+    os.makedirs(os.path.join(base_dir, "weights"), exist_ok=True)
+
     table = pq.read_table(dataloc)
-    pathsave = os.path.join(base_dir, "weights", "MRISELUresin.h5")
 
     num_samples = table.num_rows
-
-    dataset = tf.data.Dataset.from_generator(lambda: parquet_generator(table), output_signature = (tf.TensorSpec(shape = (None, None, 3), dtype = tf.float32), tf.TensorSpec(shape = (), dtype = tf.int32),),)
-    dataset = dataset.shuffle(num_samples, seed = 67, reshuffle_each_iteration = False)
-
-    train_size = int(0.8 * num_samples)
-    train = dataset.take(train_size)
-    test  = dataset.skip(train_size)
-
-    train = (train.map(preprocess, num_parallel_calls = tf.data.AUTOTUNE).batch(32).prefetch(tf.data.AUTOTUNE))
-    test = (test.map(preprocess, num_parallel_calls = tf.data.AUTOTUNE).batch(32).prefetch(tf.data.AUTOTUNE))
-
     labels = table.column("label").to_numpy()
     num_classes = int(labels.max() + 1)
+    BATCH = 32
+    train_size = int(0.8 * num_samples)
 
-    #model arch
-    def build_model(num_classes):
-        inputs = Input(shape = (128, 128, 3))
+    dataset = tf.data.Dataset.from_generator(
+        lambda: parquet_generator(table),
+        output_signature=(
+            tf.TensorSpec(shape=(None, None, 3), dtype=tf.float32),
+            tf.TensorSpec(shape=(), dtype=tf.int32),
+        ),
+    )
 
-        x = Conv2D(128, 3, activation = 'selu', padding = "same", kernel_initializer = "lecun_normal")(inputs)
+    dataset = dataset.shuffle(num_samples, seed=67, reshuffle_each_iteration=False)
+    train_ds = dataset.take(train_size)
+    val_ds = dataset.skip(train_size)
+    steps_per_epoch = train_size // BATCH
+    val_steps = (num_samples - train_size) // BATCH
 
-        x = SELUResidual(128)(x)
-        x = SELUResidual(128, stride=2)(x)
-        x = SELUInception(128)(x)
+    train_ds = (train_ds
+                .map(preprocess, num_parallel_calls=tf.data.AUTOTUNE)
+                .shuffle(2048, reshuffle_each_iteration=True)
+                .batch(BATCH)
+                .repeat()
+                .prefetch(tf.data.AUTOTUNE))
 
-        x = SELUResidual(128)(x)
-        x = SELUResidual(128, stride=2)(x)
-        x = SELUInception(128)(x)
+    val_ds = (val_ds
+              .map(preprocess, num_parallel_calls=tf.data.AUTOTUNE)
+              .batch(BATCH)
+              .prefetch(tf.data.AUTOTUNE))
 
-        x = SELUResidual(256)(x)
-        x = SELUResidual(256, stride=2)(x)
-        x = SELUInception(256)(x)
-
-        x = GlobalAveragePooling2D()(x)
-
-        outputs = Dense(num_classes, kernel_initializer="lecun_normal")(x)
-
-        return Model(inputs, outputs)
-
-    labels = table.column("label").to_numpy()
-    num_classes = len(np.unique(labels))
     model = build_model(num_classes)
+    eta_sgd = min(1e-3, lecun_lr(D=2, L_ell=1.0) / 10.0)
+
+    optimizer = tf.keras.optimizers.SGD(
+        learning_rate=eta_sgd, momentum=0.9, nesterov=True, clipnorm=1.0)
+
     loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer=optimizer, loss=loss, metrics=["accuracy"])
 
-    ES = EarlyStopping(
-        monitor = "val_loss",
-        min_delta = 0.0001,
-        patience = 16,
-        verbose = 1,
-        mode = "auto",
-        restore_best_weights = True,
-    )
-    MC = ModelCheckpoint(
-        filepath = pathsave,
-        monitor = "val_loss",
-        save_best_only = True,
-        verbose = 1,
-        save_freq = "epoch",
+    callbacks = [
+        EarlyStopping(monitor="val_loss", min_delta=0.0001, patience=16,
+                      verbose=1, restore_best_weights=True),
 
-    )
+        ModelCheckpoint(filepath=pathsave, monitor="val_loss",
+                        save_best_only=True, verbose=1, save_freq="epoch"),
+    ]
 
-    steps_per_epoch = train_size // 32
-    val_steps = (num_samples - train_size) // 32
-
-    optimizer = tf.keras.optimizers.SGD(learning_rate = 1e-3, momentum = 0.9, nesterov = True)
-
-    model.compile(optimizer = optimizer, loss = loss, metrics = ['accuracy'])
-    model.fit(train, epochs = 256, validation_data = test, steps_per_epoch=steps_per_epoch, validation_steps = val_steps, callbacks = [ES, MC],)
+    model.fit(train_ds, epochs=256, validation_data=val_ds,
+              steps_per_epoch=steps_per_epoch, validation_steps=val_steps,
+              callbacks=callbacks)
 
 if __name__ == "__main__":
     trainResinSELU()
